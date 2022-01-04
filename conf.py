652e209f11179cd3e475a553f121c2f1eba413ea
Abhay Charan das <abhay.charan.d@gmail.com>
2022-01-04 17:22:15 +0200

    added feature Index

--- conf.py
+++ conf.py
@@ -31,7 +31,8 @@ release = '2021.04.29'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions =  []
+#extensions =  []
+extensions = ['sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -58,6 +59,8 @@ today_fmt = "%Y-%m-%d"
 latex_use_xindy = True
 
 text_add_secnumbers = False
+
+
 
 #latex_maketitle = r'''
 #\input{style.tex.txt}
