42f116ba05a1f2b384ddefbfd3743aec22711623
Abhay Charan das <abhay.charan.d@gmail.com>
2022-02-16 18:19:08 +0200

    added OTF fonts

---
+++ PreprocessorEPUB.sh
@@ -0,0 +1,19 @@
+#!/bin/bash - 
+set -o nounset                              # Treat unset variables as an error
+
+DIR=_static
+
+rm -f ${DIR}/CharisSIL-*.woff
+rm -f ${DIR}/CharisSIL-*.woff2
+rm -f ${DIR}/favicon*.ico
+rm -f ${DIR}/custom.css
+rm -f ${DIR}/CharisSIL-webfont.css
+#rm -f ${DIR}/
+
+#cd _build/html
+#for STR in contents.html genindex.html glossary.html index.html search.html
+#do
+#    echo "    Processing file: $STR"
+#    sed -i 's/placeholder="Поиск в документации"/placeholder="Поиск в книге"/'  ${STR}
+#done
+
