fdd534e7759250e1b6fcc81ae329c4f232824cb8
Abhay Charan das <abhay.charan.d@gmail.com>
2022-02-17 00:53:45 +0200

    fixed : in PostprocessorEPUB.sh

--- PostprocessorEPUB.sh
+++ PostprocessorEPUB.sh
@@ -7,5 +7,9 @@ FILE=contents.xhtml
 cd $DIR
 echo "    Processing file: $FILE"
 # <p class="attribution">—Нектар преданности, Введение</p>
-sed -i 's%<p class="attribution">—\(.*\)</p>%<p class="attribution">(\1)</p>%'  ${FILE}
+sed -i 's%<p class="attribution">—\(.*\)</p>%<p class="attribution">(\1)</p>%' ${FILE}
 
+# add second ':'
+sed -zE 's%(</dt>[^\n]*\n[^\n]*<p>:)% :\1%Mg' -i ${FILE}
+# remove first ':'
+sed -i 's%<p>: %<p>%g' ${FILE}
