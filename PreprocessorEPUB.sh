17c624cc4495504e3356848f3f1175ffbf4902cc
Abhay Charan das <abhay.charan.d@gmail.com>
2022-02-18 13:06:19 +0200

    fixed for EPUB

--- PreprocessorEPUB.sh
+++ PreprocessorEPUB.sh
@@ -13,12 +13,7 @@ rm -f ${DIR}/CharisSIL-*.woff2
 rm -f ${DIR}/favicon*.ico
 rm -f ${DIR}/custom.css
 rm -f ${DIR}/CharisSIL-webfont.css
-#rm -f ${DIR}/
 
-#cd _build/html
-#for STR in contents.html genindex.html glossary.html index.html search.html
-#do
-#    echo "    Processing file: $STR"
-#    sed -i 's/placeholder="Поиск в документации"/placeholder="Поиск в книге"/'  ${STR}
-#done
-
+# Fix warnings about Menu:
+sed -i '/^[ ]*genindex$/d' ./index.rst
+sed -i '/^[ ]*glossary$/d' ./index.rst
