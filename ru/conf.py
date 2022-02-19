7de08d6acfd078b2097ea2e7b05acceeae2e07f8
Abhay Charan das <abhay.charan.d@gmail.com>
2022-02-19 11:24:52 +0200

    Multi lang support

--- ru/conf.py
+++ ru/conf.py
@@ -57,7 +57,7 @@ language = 'ru'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.git']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'ru', 'en', '_templates', 'site']
 
 # Enable figures numbering
 numfig = True
