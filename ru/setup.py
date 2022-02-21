339f089ca9e32d24815d15261b07d73460cc484b
Abhay Charan das <abhay.charan.d@gmail.com>
2022-02-21 23:57:55 +0200

    try

--- ru/setup.py
+++ ru/setup.py
@@ -9,8 +9,10 @@ from setuptools import setup
 if "install" in sys.argv:
     os.system('rm -rf en')
     os.system('mv ru/* .')
+    print(os.path.abspath('.'))
+    print('okalash {}'.format(os.path.abspath('.')))
 
 setup()
 
 
-/home/docs/checkouts/readthedocs.org/user_builds/bona-fide-guru/envs/html/bin/python -m sphinx -T -E -b html -d _build/doctrees -D language=ru . _build/html
+#/home/docs/checkouts/readthedocs.org/user_builds/bona-fide-guru/envs/html/bin/python -m sphinx -T -E -b html -d _build/doctrees -D language=ru . _build/html
